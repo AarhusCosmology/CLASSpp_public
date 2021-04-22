217f1ed84dc8e8cdeaa2c9ed0dca5cbe53643c74
EBHolm <emil.brinch@hotmail.com>
2021-04-22 17:34:36 +0200

    Another bug fix.. now should work!

--- tools/non_cold_dark_matter.cpp
+++ tools/non_cold_dark_matter.cpp
@@ -428,7 +428,8 @@ int NonColdDarkMatter::background_ncdm_init(FileContent* pfc, const NcdmSettings
   else if (log10lifetime_read == _TRUE_) {
     class_alloc(Gamma_list, N_ncdm_decay_dr_*sizeof(double), errmsg);
     for (int n = 0; n < N_ncdm_decay_dr_; n++) {
-      Gamma_list[n] = pow(10., 1./log10lifetime_list[n]/(365*24*60*60)*_Mpc_over_m_*1e-3);
+      double lifetime = pow(10., log10lifetime_list[n]);
+      Gamma_list[n] = 1./lifetime/(365*24*60*60)*_Mpc_over_m_*1e-3;
     }
   }
 
